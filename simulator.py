2d017e3965ba4112b56b6b150f160c8a8e6eb114
1999ADEK <chihjung@stanford.edu>
2023-11-16 05:02:09 +0000

    Add naive rules for updating state and action

--- simulator.py
+++ simulator.py
@@ -26,28 +26,96 @@ POSITIONS = [
 ]
 SPEED = [0, 1]
 
+speed_lookup_table = {
+    # (stroke, ball_hit):    (avg, std)
+    ("forehand", "serve"):   (30.87, 3.85),
+    ("forehand", "slice"):   (14.20, 2.04),
+    ("forehand", "topspin"): (21.86, 2.92),
+    ("forehand", "return"):  (20.26, 4.96),
+    ("backhand", "slice"):   (13.81, 3.41),
+    ("backhand", "topspin"): (21.42, 3.60),
+    ("backhand", "return"):  (19.44, 3.58),
+    ("backhand", "volley"):  (6.88,  1.60),
+    ("backhand", "stop"):    (7.54,  0.87),
+}
+
+action_success_rate = {
+    # (stroke, ball_hit):    prob
+    ("forehand", "serve"):   0.7,
+    ("forehand", "slice"):   0.7,
+    ("forehand", "topspin"): 0.7,
+    ("forehand", "return"):  0.7,
+    ("backhand", "slice"):   0.7,
+    ("backhand", "topspin"): 0.7,
+    ("backhand", "return"):  0.7,
+    ("backhand", "volley"):  0.7,
+    ("backhand", "stop"):    0.7,
+}
+
+position_lookup_table = {
+    "TL": {"TL": 0.28, "TR": 0.12, "BL": 0.36, "BR": 0.24},
+    "TR": {"TL": 0.12, "TR": 0.28, "BL": 0.24, "BR": 0.36},
+    "BL": {"TL": 0.21, "TR": 0.09, "BL": 0.49, "BR": 0.21},
+    "BR": {"TL": 0.09, "TR": 0.21, "BL": 0.21, "BR": 0.49},
+}
+
+action_probs = {
+    # (stroke, ball_hit):    prob
+    ("forehand", "serve"):   0.0, # Set to zero since it only happens at the beginning
+    ("forehand", "slice"):   0.125,
+    ("forehand", "topspin"): 0.125,
+    ("forehand", "return"):  0.125,
+    ("backhand", "slice"):   0.125,
+    ("backhand", "topspin"): 0.125,
+    ("backhand", "return"):  0.125,
+    ("backhand", "volley"):  0.125,
+    ("backhand", "stop"):    0.125,
+}
+
+action_sets = list(action_probs.keys())
 
 class Player(object):
-    def __init__(self, transition=None, action_probs=None):
-        self.transition = transition
+    def __init__(self):
+        self.speed_lookup_table = speed_lookup_table
+        self.position_lookup_table = position_lookup_table
         self.action_probs = action_probs
+        self.action_success_rate = action_success_rate
 
     def update_state(self, state, action):
-        # =========== TODO =========== #
-        # - Each state is represented as a tuple (position, speed),
-        #   which refers to the status of the ball.
-        #   (speed == 0) means the player misses the ball or the ball goes
-        #   outside the court. In this case, the opponent get one point.
-        # - Each action is represented as a tuple (stroke, shot). 
+        """
+        - Each state is represented as a tuple (position, speed),
+            which refers to the status of the ball.
+            (speed == 0) means the player misses the ball or the ball goes
+            outside the court. In this case, the opponent get one point.
+        - Each action is represented as a tuple (stroke, shot).
+        """
+
+        # Unpack current state and action
         position, speed = state
         stroke, shot = action
-        return (random.choice(POSITIONS), random.choice(SPEED))
-        # =========== TODO =========== #
+
+        # Determine the speed of the next state
+        if np.random.uniform() > self.action_success_rate.get(action, 0):
+            # Attempt fails
+            next_speed = 0
+        else:
+            mean, std = self.speed_lookup_table.get(action, (0, 0))
+            next_speed = np.random.normal(mean, std)
+        
+        # Determine the position of the next state
+        next_state = np.random.choice(
+            POSITIONS,
+            p=list(self.position_lookup_table[position].values())
+        )
+
+        return (next_state, next_speed)
 
     def choose_action(self, state=None):
-        # =========== TODO =========== #
-        return (random.choice(STROKE_TYPES), random.choice(BALL_HIT_TYPES))
-        # =========== TODO =========== #
+        action_idx = np.random.choice(
+            len(action_sets),
+            p=list(self.action_probs.values())
+        )
+        return action_sets[action_idx]
 
 
 class TennisSimulator(object):
